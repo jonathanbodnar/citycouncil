efd9d7d0c27145cf2323fd741f33988871ffdcbc
Jonathan Bodnar <jonathanbodnar@gmail.com>
2025-10-30 16:40:48 -0500

    Fix Express route pattern

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ if (process.env.PRERENDER_TOKEN) {
 app.use(express.static(path.join(__dirname, 'build')));
 
 // Handle React routing, return all requests to React app
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
