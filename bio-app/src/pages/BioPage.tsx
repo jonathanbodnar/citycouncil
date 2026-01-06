3ceb89ce8e1bd944f934b0ef3bb636043500f7ae
Jonathan Bodnar <jonathanbodnar@gmail.com>
2026-01-06 16:49:01 -0600

    Change 'Stay connected to' to 'Stay connected with'

--- bio-app/src/pages/BioPage.tsx
+++ bio-app/src/pages/BioPage.tsx
@@ -1253,7 +1253,7 @@ const BioPage: React.FC = () => {
                       color: getContrastTextColor(bioSettings?.button_color || '#3b82f6')
                     }}
                   >
-                    {subscribing ? '...' : `Stay connected to ${displayName.split(' ')[0]}`}
+                    {subscribing ? '...' : `Stay connected with ${displayName.split(' ')[0]}`}
                     {!subscribing && (
                       <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14 5l7 7m0 0l-7 7m7-7H3" />
